cf325559f7d1633bbdd884c46eab132dc15ee583
66tuna <66tuna1018@gmail.com>
2021-12-27 16:19:07 +0900

    codeit

---
+++ dicegame/src/index.js
@@ -0,0 +1,27 @@
+import ReactDOM from 'react-dom';
+
+// const product = 'MacBack';
+// const model = 'Air';
+// const item = product + model;
+
+// function handleClick() {
+//   alert('곧 도착합니다.');
+// }
+
+// ReactDOM.render(
+//   <>
+//     <h1>나만의 {item} 주문하기</h1>
+//     <button onClick={handleClick}>확인</button>
+//   </>,
+//   document.getElementById('root')
+// );
+
+import App from './App';
+
+ReactDOM.render(
+  <App/>  
+  ,
+  document.getElementById('root')
+)
+
+
